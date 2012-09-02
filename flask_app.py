b03a1a34261cd7942cb0ac7a2a1bfff42a981c3c
Rahul <rahul.rajan@gmail.com>
2012-09-02 14:17:49 -0700

    Finishing ending condition

--- flask_app.py
+++ flask_app.py
@@ -16,21 +16,24 @@ WAITING = []
 AGREE   = {}
 ORDERS  = {}
 LAST_ANSWER = {}
+CORRECT_N = {}
+TOTAL_N = 8
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if   request.method == 'GET':
+    if   request.method == 'GET':    #- to load the page -#
         usrID = newUsr()
         PLAYERS.append(usrID)
         resp  = flask.make_response(flask.render_template('index.html'))
         resp.set_cookie('username', str(usrID))
         return resp
 
-    elif request.method == 'POST':
+    elif request.method == 'POST':   
         usrID = getUsr(request) 
-        if   'init'    in request.form:
+        if   'init'    in request.form:    #- for init() -#
             return flask.jsonify( paired = addUsr(usrID) )
-        elif 'waiting' in request.form:
+        elif 'waiting' in request.form:    #- for recvAgree() -#
             print 'WAITING: ' + usrID 
             paired = getEnemy(usrID)
             return flask.jsonify( paired = paired )
@@ -39,7 +42,7 @@ def index():
 @app.route('/shuffle', methods=['GET', 'POST'])
 def shuffle():
     global ORDERS
-    if   request.method == 'GET':
+    if   request.method == 'GET': #- for recvOrder() -#
 
         print 'shuffle GET' + getUsr(request)
         enemyID = getEnemy(getUsr(request))
@@ -50,7 +53,7 @@ def shuffle():
         else:
             return flask.jsonify( success=0 )
 
-    elif request.method == 'POST':
+    elif request.method == 'POST':    #- for sendOrder() -#
         print 'shuffle POST' + getUsr(request)
         ORDERS[getUsr(request)] = request.form.getlist('order')
         return flask.make_response()
@@ -58,11 +61,12 @@ def shuffle():
 
 @app.route('/answer', methods=['GET', 'POST'])
 def answer():
-    global LAST_ANSWER
-    if   request.method == 'GET':
+    global LAST_ANSWER, TOTAL_N, CORRECT_N
+    usrID   = getUsr(request)
+    enemyID = getEnemy(usrID)
+    if   request.method == 'GET':    #- for recvAns() -#
 
-        print 'answer GET' + getUsr(request)
-        enemyID = getEnemy(getUsr(request))
+        print 'answer GET' + usrID
         print LAST_ANSWER
         if    enemyID in LAST_ANSWER:
             img0 = LAST_ANSWER[enemyID]['img0']
@@ -70,22 +74,47 @@ def answer():
             img1 = LAST_ANSWER[enemyID]['img1']
             box1 = LAST_ANSWER[enemyID]['box1']
             del LAST_ANSWER[enemyID]
-            return flask.jsonify( img0=img0, box0=box0, img1=img1, box1=box1, success=1 )
-        else:
-            return flask.jsonify( img0="",   box0="",   img1="",   box1="",   success=0 )
 
-    elif request.method == 'POST':
-        print 'answer POST' + getUsr(request)
-        ans = {}
-        
+            if gameOver(usrID, enemyID):
+                return flask.jsonify( img0=img0, box0=box0, img1=img1, box1=box1, success=1, end=1 )
+            else:
+                return flask.jsonify( img0=img0, box0=box0, img1=img1, box1=box1, success=1, end=0 ) 
+        else:
+            return flask.jsonify( img0="",   box0="",   img1="",   box1="",   success=0, end=0 )
+
+    elif request.method == 'POST':    #- for sendAns() -#
+
+        print 'answer POST' + usrID
+        ans = {} 
         ans['img0'] = request.form.get('img0')
         ans['box0'] = request.form.get('box0')
         ans['img1'] = request.form.get('img1')
         ans['box1'] = request.form.get('box1')
+        LAST_ANSWER[usrID] = ans
+        print ans
 
-        LAST_ANSWER[getUsr(request)] = ans
-        return flask.make_response()
+        CORRECT_N[usrID] = request.form.get('correctN')
+        
+        if gameOver(usrID, enemyID): 
+            return flask.jsonify( end='1' )
+        else:
+            return flask.jsonify( end='0')
 
+
+
+def gameOver(usrID, enemyID):
+    if ( usrID not in CORRECT_N ) or (enemyID not in CORRECT_N):
+        print 'no no'
+        return False
+
+    print CORRECT_N
+    if  ( int(CORRECT_N[usrID]) + int(CORRECT_N[enemyID]) == TOTAL_N ):
+        print 'Game Over'
+    else:
+        print str(CORRECT_N[usrID]) + ':' + str(CORRECT_N[enemyID])
+
+    return ( int(CORRECT_N[usrID]) + int(CORRECT_N[enemyID]) == TOTAL_N )
+ 
 
 
 def addUsr(usrID):
@@ -139,4 +168,5 @@ def getEnemy(usrID):
 if __name__ == "__main__":
     port = sys.argv[1]
     app.run(debug=True, host="209.129.244.8", port=int(port), threaded=True )
+
 
